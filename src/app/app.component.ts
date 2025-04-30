2a6380694c143567f357d39dc29ddd47c2d93990
Javier.rc <javier.rodriguez.external@eviden.com>
2025-04-30 14:08:30 +0200

    cambios 30.04.25_14.30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface Entrada {
+  titulo: string;
+  }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de usuarios';
+  mensaje = "";
+  registrado = false;
+  nombre: string = "";
+  apellidos: string = "";
+  entradas: Entrada[];
+
+  constructor(){
+    this.entradas = [
+      {titulo: "java experto"},
+      {titulo: "php intermedio"},
+      {titulo: "javaScript con proyecto"},
+      {titulo: "c++ experto"},
+      {titulo: "spring + angular experto"},
+    ]
+  }
+
+  registrarUsuario(){
+    this.registrado = true;
+    this.mensaje = "usuario registrado con exito";
+  }
 }
